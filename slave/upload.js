6c2e29544007254504c88b621fa4b74f7054a601
Tom Barwick <tom_barwick@hotmail.com>
2017-05-15 15:25:46 +0100

    Adds gdrive endpoint

--- slave/upload.js
+++ slave/upload.js
@@ -1,16 +1,26 @@
 var spawn = require('child_process').spawn;
 
 var ACD_REMOTE = 'acd:';
+var GDRIVE_REMOTE = 'gdrive:uploads';
 
 function run(filename) {
-  var proc = spawn('rclone', ['sync', filename, ACD_REMOTE]);
+  var acd = spawn('rclone', ['sync', filename, ACD_REMOTE]);
+  var gdrive = spawn('rclone', ['sync', filename, GDRIVE_REMOTE]);
   console.log(`Uploading: ${filename}`);
 
-  proc.on('exit', function(code) {
+  acd.on('exit', function(code) {
     if (code === 0) {
-      console.log('rclone: success');
+      console.log(`rclone: success => ${ACD_REMOTE}`);
     } else {
-      console.log('rclone: error');
+      console.log(`rclone: error => ${ACD_REMOTE}`);
+    }
+  });
+
+  gdrive.on('exit', function(code) {
+    if (code === 0) {
+      console.log(`rclone: success => ${GDRIVE_REMOTE}`);
+    } else {
+      console.log(`rclone: error => ${GDRIVE_REMOTE}`);
     }
   });
 }
